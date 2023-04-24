f1c6b43a14c83cd6326ef408514b95e17e1a0fe1
Kondauciel <luis.molina08@unach.mx>
2023-04-24 01:40:13 -0600

    mejora importante

--- pago.php
+++ pago.php
@@ -4,7 +4,8 @@
     $cantidad = $_POST['cantidad'];
     $tarjeta = $_POST['tarjeta'];
     
-    echo "¡Muchas gracias por tu ayuda ".$nombre."!";
-    echo "Tu donativo de".$cantidad." ayudará a muchos animalitos.";
-    echo "El recibo del cobro a tu tarjeta ".$tarjeta." se enviará a tu correo ".$correo." en un maximo de 24 horas.";
+    echo "¡Muchas gracias por tu ayuda ".$nombre."!\n";
+    echo "Tu donativo de ".$cantidad." ayudará a muchos animalitos.\n";
+    echo "El recibo del cobro a tu tarjeta ".$tarjeta." se enviará a tu correo ".$correo." en un maximo de 24 horas.\n";
+    echo "Atentamente: El equuipo de guamiau.";
 ?>
--- script.sql
+++ script.sql
@@ -4,5 +4,10 @@ create table Productos(
     Id int PRIMARY key AUTO_INCREMENT,
     Nombre varchar(120),
     Correo varchar(120),
-    clave varchar(120)
+    clave varchar(120),
+    Telefono int (10),
+    Estado varchar(120),
+    Municipio varchar(120),
+    Colonia varchar(120),
+    CodigoPostal int(10)
 );
