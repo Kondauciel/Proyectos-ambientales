aba05e4fc1fb1de3a5a8af80ad2ed6c8dbca471f
Kondauciel <luis.molina08@unach.mx>
2023-04-24 12:42:32 -0600

    casi listo

--- registro.php
+++ registro.php
@@ -4,17 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>Nuevo Usuario</title>
 </head>
 <body>
+    <header>
+        <div id="title">
+            <img src="img/logo_Pet.jpg" alt="mi logo" id="logo">
+            <h1>GUAUMIAU</h1>
+        </div>
+    </header>
     <form action="guardarUsuario.php" method="post">
         <label>Nombre:</label>
-        <input type="text" name="nombre">
+        <input type="text" name="Nombre"> <br>
         <label >Correo:</label>
-        <input type="text" name="precio">
+        <input type="text" name="Correo"><br>
         <label >Contraseña:</label>
-        <input type="number" name="existencias">
-        <input type="submit" value="Guardar">
+        <input type="text" name="Clave"><br>
+        <label >Telefono:</label>
+        <input type="number" name="Telefono"><br>
+        <label >Estado:</label>
+        <input type="text" name="Estado"><br>
+        <label >Municipio:</label>
+        <input type="text" name="Municipio"><br>
+        <label >Colonia:</label>
+        <input type="text" name="Colonia"><br>
+        <label >Codigo Postal:</label>
+        <input type="number" name="Codigo postal"><br>
+        <input type="submit" value="Quiero registrarme">
     </form>
+    <tbody>
+        <?php
+            require_once 'Models/usuario.php';
+            $producto=new Usuario();
+            $resultado=$producto->ObtenerUsuario();
+            if(count($resultado)>0){
+                foreach ($resultado as $registro) {
+                    echo '<tr>';
+                    echo '<td>'.$registro['Id'].'</td>';
+                    echo '<td>'.$registro['Nombre'].'</td>';
+                    echo '<td>'.$registro['Correo'].'</td>';
+                    echo '<td>'.$registro['Clave'].'</td>';
+                    echo '<td>'.$registro['Estado'].'</td>';
+                    echo '<td>'.$registro['Municipio'].'</td>';
+                    echo '<td>'.$registro['Colonia'].'</td>';
+                    echo '<td>'.$registro['CodigoPostal'].'</td>';
+                    echo '</tr>';
+                }
+            }
+        ?>
+    </tbody>
 </body>
 </html>
